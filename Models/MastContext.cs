1017231143adb9ded2db91f8efdcc8d92a26f5cd
RW-Vaibhav <vaibhav.bhosale@rewiseglobal.com>
2024-05-24 23:01:52 +0530

    connection string removed from the context file

--- Models/MastContext.cs
+++ Models/MastContext.cs
@@ -101,10 +101,6 @@ public partial class MastContext : DbContext
 
     public virtual DbSet<WorkModeMaster> WorkModeMasters { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-22O7208;Initial Catalog=MAST;Integrated Security=True;MultipleActiveResultSets=true;User ID=sa;Password=etech123;TrustServerCertificate=true");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AttendanceActionStatus>(entity =>
