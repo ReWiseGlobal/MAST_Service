26dbf60d791fcdf0d2bacea8202bed4a4f8eb507
Suhashk490 <Suhas.Kamble@rewiseglobal.com>
2024-08-02 15:19:53 +0530

    Hour change

--- ReleaseDateReminderService.cs
+++ ReleaseDateReminderService.cs
@@ -113,7 +113,7 @@ namespace MAST_Service
                                 {
                                     DateTime notificationDate = releaseDate.AddDays(-daysBefore);
 
-                                    if (now.Date == notificationDate.Date && now.Hour == 9)
+                                    if (now.Date == notificationDate.Date && now.Hour == 13)
                                     {
                                         if (hrActionTaken)
                                         {
